17d5ed8ffb4c490b6516d4367eacd20e94a42eaa
tramvirtualis <ngoctramngff4@gmail.com>
2025-08-18 15:04:28 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hi, My Name Is Ngoc Tram</h1>
+      <h2>MSSV: 22110439</h2>
+      <img 
+        src="/image1.jpg" 
+        alt="Ảnh cá nhân" 
+        style={{ width: "200px", borderRadius: "50%" }}
+      />
+      <p>Mình là sinh viên năm 4 ngành Công nghệ thông tin</p>
+    </div>
+  );
+}
+
+export default App;
